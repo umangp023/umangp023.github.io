3ab0035b0001453d6498e4024951d091494cdee1
Umang Patel <47998115+umangp023@users.noreply.github.com>
2024-09-17 23:12:14 -0400

    Add files via upload

---
+++ public/webImages.js
@@ -0,0 +1,11 @@
+// public/webImages.js
+import web1 from './web1.png';
+import web2 from './web2.png';
+import web3 from './web3.png';
+import web4 from './web4.png';
+import web5 from './web5.png';
+import web6 from './web6.png';
+
+const webImages = [web1, web2, web3, web4, web5, web6];
+
+export default webImages;
